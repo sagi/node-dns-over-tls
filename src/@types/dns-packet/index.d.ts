5a4888a498c37b88979f25ac8644c4f0b1d439c3
Sagi Kedmi <sagi@users.noreply.github.com>
2018-08-30 06:23:31 +0300

    an attempt at module typescript definition

--- src/@types/dns-packet/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'dns-packet' {
-  export const RECURSION_DESIRED: number;
-  export function streamDecode(response: Buffer): object;
-  export function streamEncode(dnsQuery: object): Buffer;
-}
